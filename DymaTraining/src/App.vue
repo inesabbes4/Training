eee2e65696cdfa142d838bf2517953c91902a9a6
Ines Abbes <ines.abbes@sast-mfa.com>
2023-07-13 12:18:23 +0200

    init

---
+++ DymaTraining/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 v-bind="obj">Bonjour {{ `${name} je concatène` }} </h1>
+  <input :type="getType()">
+</template>
+
+<script setup lang="ts">
+import { VueElement, createApp } from 'vue';
+
+
+  const h1title = 'montitre2'
+  const h1id = 'monid2'
+  const name = 'Dyma'
+
+  const obj = {
+    'class' : h1title,
+    'id' : h1id
+  }
+
+  function getType() {
+    return 'date'
+  }
+
+  const vue = createApp({});
+
+</script>
+
+<style></style>
